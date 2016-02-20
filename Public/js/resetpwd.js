9fd403ac1dedc053ac6820e65adad7b028b888c2
cccccdn <524749923@qq.com>
2016-02-20 21:17:05 +0800

    Update

--- Public/js/resetpwd.js
+++ Public/js/resetpwd.js
@@ -2,18 +2,32 @@
  * Created by danni on 2016/2/20.
  */
 
-function check(){
-        var new_pwd = document.getElementById("new_pwd").value;
-        var new_pwd2 = document.getElementById("new_pwd2").value;
-        if(new_pwd != new_pwd2){
-            alert("两次密码不同");
-            //document.getElementById("respond").innerHTML="两次密码不一致"
-            if(event.preventDefault()){
-                event.preventDefault();
-            }else{
-                event.returnValue = false;
-            }
+$(document).ready(function(){
+    $("#new_pwd2").blur(function(){
+        var $new_pwd = $("#new_pwd").val();
+        var $new_pwd2 = $("#new_pwd2").val();
+        if($new_pwd != $new_pwd2){
+            $("#respond").show();
+            //document.getElementById("rset").isDisabled = true;
+            $("#rset").attr("disabled", true);
+
+
         }else{
+            $("#respond").hide();
+            //document.getElementById("rset").isDisabled = false;
+            $("#rset").attr("disabled", false);
             return true;
         }
-}
+    });
+});
+
+$(document).ready(function(){
+    $("#rset").click(function(){
+        if ($("#new_pwd").val()==""){
+            $("#rset").attr("disabled", true);
+        }else
+        {
+            $("#pwdform").submit();
+        }
+    });
+});
