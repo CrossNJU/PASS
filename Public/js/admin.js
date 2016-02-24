da15741a1703e641f831e9d4556880ba52d80da4
danniChen`` <524749923@qq.com>
2016-02-24 17:48:51 +0800

    Admin

--- Public/js/admin.js
+++ Public/js/admin.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
         $(this).siblings('.test').children('.test-1').toggle();
     });
 });
+
+$(document).ready(function(){
+    $('#btn-add-stu').click(function(){
+        window.location.href="student_add";
+
+    })
+})
+
+$(document).ready(function(){
+    $('#btn-add-cou').click(function(){
+        window.location.href="course_add";
+
+    })
+})
+
+$(document).ready(function(){
+    $('#btn-add-tea').click(function(){
+        window.location.href="teacher_add";
+
+    })
+})
