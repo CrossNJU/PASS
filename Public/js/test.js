6458df0580cdb084baed1b86455e21c40b9cc15a
RickChem <1395314348@qq.com>
2016-02-24 10:42:00 +0800

    Modify something

---
+++ Public/js/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by raychen on 16/2/21.
+ */
+
+$(document).ready(function(){
+
+    $('.up').click(function(){
+        $(this).siblings('.test').children('.test-1').toggle();
+    });
+
+});
