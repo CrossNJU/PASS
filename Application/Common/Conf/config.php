f767120316445d20f0f3b9efb9b9cce965ed58ae
cr14 <cr14@software.nju.edu.cn>
2016-03-11 20:42:03 +0800

    Add some

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -23,4 +23,8 @@ return array(
     'TAGLIB_END'=>'}>',
 
     'URL_BASE' => './Public/uploads/',
+    'URL_HEAD' => 'http://localhost/PASS/',
+
+    'URL_MODEL'             =>  2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
 );
