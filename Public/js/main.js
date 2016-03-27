b77b81ef21359a5e6985085d15b6e23b195d1ff2
chenmuen <593827475@qq.com>
2016-03-27 20:13:34 +0800

    Add feedback page

--- Public/js/main.js
+++ Public/js/main.js
@@ -14,13 +14,17 @@ $(document).ready(function (){
     $(document).on("click",".dialog .btn",closeFunc);
     $(document).on("click",".modal-wrapper .close-btn",closeFunc);
 
+    //添加意见反馈窗口的监听
+    $(document).on("click",".feedback-btn",function() {
+        showFeedbackDialog();
+    });
+
     //设置页面最小高度
     minHeight();
     $(window).resize(minHeight);
 
     $(".rect").each(function() {
         var height = $(this).parent().height();
-        console.log($(this).parent().height());
         $(this).css("height",height+"px");
     });
 
@@ -47,20 +51,67 @@ function showDialog(text){
     $("body").prepend("<div class='modal-wrapper dialog-wrapper'> <div class='dialog'> <p class='warning-text'>"+text+"</p> <a href='javascript:void(0)' class='btn confirm-btn'>确认</a> <a href='javascript:void(0)' class='btn btn-active'>取消</a> </div> </div>");
     $('.dialog-wrapper').fadeIn();
     $("body").css("overflow","hidden");
-    function cancelFunc (){
-        $(".dialog-wrapper").fadeOut(function(){
-            $("body").css("overflow","auto");
-            $(".modal-wrapper").remove();
-        });
-    };
-    $(".dialog .btn").on("click",cancelFunc);
-    $(".dialog .close-btn").on("click",cancelFunc);
+    $(".dialog .btn").on("click",closeFunc);
+    $(".dialog .close-btn").on("click",closeFunc);
 }
+
+function showFeedbackDialog() {
+
+    $("body").append("" +
+        "<div class='modal-wrapper feedback-wrapper'>" +
+            "<div class='card feedback-card'>" +
+                "<div class='card-info'>" +
+                    "<h3 class='card-title'>意见反馈</h3>" +
+                    "<a class='close-btn' href='javascript:void(0)'><b></b></a> " +
+                    "<div id='feedback-content'> " +
+                    "<textarea class='textarea' id='feedback-textarea' placeholder='感谢你对哲学系作业提交系统的支持与关注，请留下你的宝贵信息帮助我们做进一步的改进'></textarea> " +
+                    "</div> " +
+                    "<a class='btn btn-primary feedback-confirm-btn'>提交反馈</a> " +
+                    "<a class='btn cancel-btn'>取消反馈</a> " +
+                "</div> " +
+            "</div> " +
+        "</div>");
+
+    var feedback = $("#feedback-textarea").val();
+
+    $(".feedback-confirm-btn").click(function () {
+        $(".feedback-confirm-btn").addClass(".disabled-btn");
+
+        if(!$(".feedback-btn").hasClass(".disable-btn")) {
+            $.ajax({
+                url:"PASS/Common/feedback",
+                data:"feedback="+feedback,
+                success:function(msg) {
+                    if(msg == 1){
+                        closeFunc();
+                        showStateBar("success","谢谢您的反馈");
+                    }else {
+                        fail();
+                    }
+                },
+                error: function() {
+                    fail();
+                }
+            });
+        }
+        function fail() {
+            showStateBar("danger","反馈失败，请稍后再试");
+            $(".feedback-confirm-btn").removeClass(".disabled-btn");
+        }
+    });
+
+
+    $(".feedback-wrapper").fadeIn();
+    $("body").css("overflow","hidden");
+
+    $(".feedback-wrapper .cancel-btn").click(closeFunc);
+}
+
 
 function closeFunc (){
     $(".modal-wrapper").fadeOut(function(){
         $("body").css("overflow","auto");
-        $(".dialog-wrapper").remove();
+        $(".dialog-wrapper,.feedback-wrapper").remove();
     });
 }
 
@@ -69,3 +120,4 @@ function minHeight(){
     var minHeight = window.innerHeight - 280;
     $(".content").css("minHeight",minHeight+"px");
 }
+
